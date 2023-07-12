7da0ff6fcf9802b214dbdedbb5bd2216dd71986f
Brian <134489011+Brikita@users.noreply.github.com>
2023-07-12 08:07:18 +0300

    final commit

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Footer() {
-    return (
-        <div>
-            <div>This is where the footer will be</div>
-        </div>
-    )
-}
--- frontend/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Header() {
-    return (
-        <header className="page--title">
-            <h1>Profile</h1>
-        </header>
-    )
-}
