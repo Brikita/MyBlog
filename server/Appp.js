e1c633372ee5fe832896288e4cc66ff4135564fa
AlvinMuiru <alvinamuiru2022@gmail.com>
2023-07-14 20:47:06 +0300

    "Creating home page to display posts ."

---
+++ server/Appp.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require("cors")
+
+const app = express();
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/x', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+  const blogPostSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    image: String,
+    
+  });
+
+  const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+ const bp= new BlogPost ({title :'Post 2', content: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+image: "https://p1.pxfuel.com/preview/933/927/598/wallpaper-gaming-game-play-technology-fun.jpg"})
+ bp.save().then(() => console.log("Blog post saved"))
+
+ 
+ app.get('/blog/posts', (req, res) => {
+    BlogPost.find({})
+    .then(BlogPosts => {
+      res.status(200)
+      .json(BlogPosts)
+      console.log(BlogPosts)
+    })
+    .catch(error => {
+      res.send(error)
+    });
+    
+  });
+   app.delete('/blog/posts/:id', (req, res) => {
+    BlogPost.findOneAndDelete({ _id: req.params.id })
+  .then(deletedPost => {
+    if (!deletedPost) {
+      return res.status(404).json({ success: false, msg: `No post with id ${req.params.id}` });
+    }
+    
+    return BlogPost.find()
+      .then(updatedPosts => {
+        return res.status(200).json({ success: true, data: updatedPosts });
+      })
+      .catch(error => {
+        return res.status(500).json({ success: false, msg: 'Error fetching updated posts', error });
+      });
+  })
+  .catch(error => {
+    return res.status(500).json({ success: false, msg: 'Error deleting post', error });
+  });
+  })
+    
+  // Start the server
+  const port =3024;
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  })
--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,56 +1,89 @@
-import React, { useState } from 'react';
+import React from 'react'
 import "./blog.css"
+import BlogPost from './BlogPost';
+import  { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const BlogPost = ({ post, onLike, onAddComment }) => {
-  const [comment, setComment] = useState('');
 
-  const handleLikeClick = () => {
-    onLike(post.id);
-  };
 
-  const handleCommentChange = (e) => {
-    setComment(e.target.value);
-  };
+export default function Blog() {
+    const [blogPosts, setBlogPosts] =useState( [
+      ]);
 
-  const handleCommentSubmit = (e) => {
-    e.preventDefault();
-    if (comment.trim() !== '') {
-      onAddComment(post.id, comment);
-      setComment('');
-    }
-  };
+      useEffect(() => {
+        fetchBlogPosts();
+      }, []);
+    
+      const fetchBlogPosts = async () => {
+        try {
+          const response = await axios.get('http://localhost:3024/blog/posts');
+          setBlogPosts(response.data);
+        } catch (error) {
+          console.log('Error fetching blog posts:', error);
+        }
+      };
+    
 
-  return (
-    <div className="blog-post">
-      <h2>{post.title}</h2>
-      <p>{post.content}</p>
+      const [search, setSearch]= React.useState('')
+      const [searchResults, setSearchResults] = React.useState([])
+      React.useEffect(
+          () => {
+            const filteredResults = blogPosts.filter((post) => post.content.toLowerCase().includes(search.toLowerCase())
+            || post.title.toLowerCase().includes(search.toLowerCase()));
+        
+  
+              setSearchResults(filteredResults.reverse())
+          },[blogPosts, search])
+      
+        const handleLike = (postId) => {
+          setBlogPosts((prevPosts) =>
+            prevPosts.map((post) => {
+              if (post.id === postId) {
+                return { ...post, likes: post.likes + 1 };
+              }
+              return post;
+            })
+          );
+        };
+      
+        const handleAddComment = (postId, comment) => {
+          setBlogPosts((prevPosts) =>
+            prevPosts.map((post) => {
+              if (post.id === postId) {
+                return { ...post, comments: [...post.comments, comment] };
+              }
+              return post;
+            })
+          );
+        };
+      
+    return (
+        <div className='Blog--Container'>
+            <div className='searchBar'>
+            <form className='searchForm' 
+                  onSubmit={function(event){
+                     event.preventDefault()
+                   }}>
+                   <input
+                   className='searchInput'
+                   id='search'
+                   type="text"
+                   placeholder='Search for'
+                   onChange={function(event){
+                    setSearch(event.target.value)
+                   }}
+                   />
+                   <button className='searchbutton'><i className='fa fa-search'></i></button>
+           </form>
+           </div>
 
-      <div className="image-container">
-
-      <img src={post.image} alt="Blog Post" className='image'/>
-
+        <h1>Welcome to My Blogging Website</h1>
+        <div className="blog-posts">
+        {searchResults.map((post) => (
+        <BlogPost key={post.id} title={post.title} content={post.content} image={post.image}  />
+      ))}
+        </div>
+       
       </div>
-
-      <div>
-        Likes: {post.likes}
-        <button className='likeButton' onClick={handleLikeClick}><i className='fa fa-heart'></i></button>
-      </div>
-      <div>
-        <form onSubmit={handleCommentSubmit}>
-          <input type="text" value={comment} onChange={handleCommentChange} placeholder="Add a comment" />
-          <button className='commentButton' type="submit"><i className="fa fa-paper-plane"></i></button>
-        </form>
-      </div>
-      <div className="comments">
-        {post.comments.map((comment, index) => (
-          <p key={index}>{comment}</p>
-        ))}
-
-
-
-      </div>
-    </div>
-  );
-};
-
-export default BlogPost;
+    )
+}
