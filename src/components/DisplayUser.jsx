bf05840cfa5158974b32a5a073f85de5af0a2fb8
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-07-14 18:33:10 +0300

    the display page

---
+++ src/components/DisplayUser.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import http from '../lib/http'
+import { useParams, Link } from 'react-router-dom'
+import './DisplayUser.css'
+
+
+function DisplayUser() {
+    const { id: userId } = useParams()
+    //const userId = '64b134853257190d7193809c'
+    const [user, setUser] = useState({})
+
+   useEffect( () => {
+        async function fetchUser(){
+            const { data } = await http.get(`/api/people/profile/${userId}`) 
+            setUser(data)
+            console.log(data)
+        }
+        fetchUser()
+    }, [])
+  return (
+    <div>
+        <h2 className='profileUserName'>{user.userName}</h2>
+        <p className='profileDescription'>{user.description}</p> 
+        <button className='profileEditButton'><Link to={`/editProfile/${userId}`} className='profileEditLink'>Edit Profile</Link></button>
+    </div>
+  )
+}
+
+export default DisplayUser
