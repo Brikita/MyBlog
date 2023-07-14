e13c3ad81dab9f417c05c2d2ad07299d5166bb24
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-07-14 18:30:36 +0300

    creating the editing page

---
+++ src/components/EditProfile.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react'
+import http from '../lib/http'
+import { useForm } from 'react-hook-form'
+import { useParams, useNavigate } from 'react-router-dom' 
+import './EditProfile.css'
+
+const EditProfile = () => {
+    const { id: userId } = useParams()
+    const navigate = useNavigate()
+    const { register, handleSubmit, reset } = useForm()
+    useEffect( () => {
+        async function fetchUser(){
+            const { data } = await http.get(`/api/people/profile/${userId}`) 
+            reset(data)
+            console.log(data)
+        }
+        fetchUser()
+    }, [])
+    const onSubmit = async ({ userName, description }) => {
+    const payload = {
+        userName,
+        description
+    }
+
+    await http.put(`/api/people/editProfile/${userId}`, {data:payload})
+    
+    navigate(`/profile/${userId}`)
+}   
+    
+
+  return (
+    <div >
+        <form onSubmit={handleSubmit(onSubmit)} className='editProfileDiv'>
+            <label htmlFor="userName" className='editProfileNameLabel'>Name:</label>
+            <input 
+            type="text" 
+            name='userName' 
+            id='userName'
+            {...register('userName')}
+            className='editProfileBox'/>
+            <label htmlFor="description" className='editProfileDescriptionLabel'>Description:</label>
+            <textarea 
+            type="text" 
+            name='description' 
+            id='description'
+            {...register('description')}
+            className='editProfileBox editProfileDescriptionBox'
+            />
+            <button type='submit' className='editProfileBtn'>Edit</button>
+        </form>
+    </div>
+  )
+}
+
+export default EditProfile
+
